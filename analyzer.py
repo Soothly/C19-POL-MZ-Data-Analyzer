f9548b67ddec47b91a6c59ab1a711640186e7fe8
soothly <gabrys.piotr@interia.pl>
2021-09-11 15:16:12 +0200

    code cleanup, PEP8 adherence

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,4 @@
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, SUPPRESS
-import csv
-import datetime
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -14,8 +12,11 @@ columns_to_plot = [
 ]
 
 def parse_args():
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, epilog="")
-    parser.add_argument("territory", help="")
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("territory", 
+        help="Input territory ID for which you want graphs generated")
     return parser.parse_args()
 
 def plot_columns(dataset, columns):
@@ -41,9 +42,13 @@ if(__name__ == "__main__"):
             continue
         filepath = data_path + "/" + file
         try:
-            data_from_file = pd.read_csv(filepath, delimiter=delimiter, encoding='utf-8')
+            data_from_file = pd.read_csv(
+                filepath, delimiter=delimiter, encoding='utf-8'
+            )
         except UnicodeDecodeError:
-            data_from_file = pd.read_csv(filepath, delimiter=delimiter, encoding='cp1250')
+            data_from_file = pd.read_csv(
+                filepath, delimiter=delimiter, encoding='cp1250'
+            )
         data.append(data_from_file)
     
     output = pd.concat(data)
@@ -52,18 +57,36 @@ if(__name__ == "__main__"):
     placeholder = output.loc[output['teryt'] == args.territory]
     single_area = placeholder.copy()
 
-    print(single_area.columns)
     single_area.dropna(axis=0, how='any', inplace=True)
-    single_area['liczba_przypadkow_int'] = single_area['liczba_przypadkow'].astype(int)
+    single_area['liczba_przypadkow_int'] = (
+        single_area['liczba_przypadkow'].astype(int)
+    )
     single_area['zgony_int'] = single_area['zgony'].astype(int)
-    single_area['przypadki_srednia_7dni'] = single_area['liczba_przypadkow_int'].rolling(window=7).mean()
-    single_area['zgony_srednia_7dni'] = single_area['zgony_int'].rolling(window=7).mean()
-    single_area['liczba_wykonanych_testow_srednia_7dni'] = single_area['liczba_wykonanych_testow'].rolling(window=7).mean()
-    single_area['stan_rekordu_na_str'] = single_area['stan_rekordu_na'].astype(str)
-    single_area['procent_poz_testow'] = (single_area['liczba_testow_z_wynikiem_pozytywnym'] / single_area['liczba_wykonanych_testow'] * 100).astype(float)
-    single_area['procent_poz_testow_7dni'] = single_area['procent_poz_testow'].rolling(window=7).mean()
-    single_area['liczba_osob_objetych_kwarantanna'] = single_area['liczba_osob_objetych_kwarantanna'].astype(int)
-    single_area['stan_rekordu_na_str'] = single_area['stan_rekordu_na_str'].astype("datetime64")
+    single_area['przypadki_srednia_7dni'] = (
+        single_area['liczba_przypadkow_int'].rolling(window=7).mean()
+    )
+    single_area['zgony_srednia_7dni'] = (
+        single_area['zgony_int'].rolling(window=7).mean()
+    )
+    single_area['liczba_wykonanych_testow_srednia_7dni'] = (
+        single_area['liczba_wykonanych_testow'].rolling(window=7).mean()
+    )
+    single_area['stan_rekordu_na_str'] = (
+        single_area['stan_rekordu_na'].astype(str)
+    )
+    single_area['procent_poz_testow'] = (
+        (single_area['liczba_testow_z_wynikiem_pozytywnym'] / 
+         single_area['liczba_wykonanych_testow'] * 100).astype(float)
+    )
+    single_area['procent_poz_testow_7dni'] = (
+        single_area['procent_poz_testow'].rolling(window=7).mean()
+    )
+    single_area['liczba_osob_objetych_kwarantanna'] = (
+        single_area['liczba_osob_objetych_kwarantanna'].astype(int)
+    )
+    single_area['stan_rekordu_na_str'] = (
+        single_area['stan_rekordu_na_str'].astype("datetime64")
+    )
     single_area.set_index('stan_rekordu_na_str', inplace=True)
 
     for column in columns_to_plot:

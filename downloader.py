e52ffa1299f8345171b6e20682f01dcd9d584c87
soothly <gabrys.piotr@interia.pl>
2021-10-08 22:18:20 +0200

    add function to remove file

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import urllib.request
 import zipfile
+import os
 
 def download_current_data(path, filename):
     urllib.request.urlretrieve(
@@ -9,3 +10,17 @@ def download_current_data(path, filename):
 def extract_data(zip_path, data_dir):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(data_dir)
+
+def remove_downloaded_archive(file_path):
+    try:
+        os.remove(file_path)
+        print("File - {} - removed".format(file_path))
+    except FileNotFoundError:
+        print("Could not remove - {}".format(file_path))
+        print("File does not exist!")
+    except IsADirectoryError:
+        print("Could not remove - {}".format(file_path))
+        print("Target is a directory")
+    except Exception:
+        print("Could not remove - {}".format(file_path))
+        print("File is likely in use")

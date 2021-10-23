fcff5a18198e436b9ebfde4b75226fb0b0ac03b0
soothly <gabrys.piotr@interia.pl>
2021-10-23 21:01:23 +0200

    refactor plotting and dataset configuration

--- analyzer.py
+++ analyzer.py
@@ -4,85 +4,21 @@ from lib.file_handler import FileHandler
 from lib.territory import Territory
 from datetime import date, datetime
 import os
-import matplotlib.pyplot as plt
-
-columns_to_plot = [
-    ['przypadki_srednia_7dni'],
-    ['zgony_srednia_7dni'],
-    ['procent_poz_testow_7dni', 'liczba_wykonanych_testow_srednia_7dni'],
-    ['liczba_osob_objetych_kwarantanna'],
-    ['liczba_na_10_tys_mieszkancow_7dni']
-]
-
-columns_to_int = [
-    ("liczba_przypadkow_int", "liczba_przypadkow"),
-    ("zgony_int", "zgony"),
-    ("liczba_osob_objetych_kwarantanna", "liczba_osob_objetych_kwarantanna")
-]
-columns_to_str = [
-    ("stan_rekordu_na_str", "stan_rekordu_na")
-]
-columns_to_average = [
-    ("przypadki_srednia_7dni", "liczba_przypadkow_int"),
-    ('zgony_srednia_7dni', "zgony_int"),
-    ('procent_poz_testow_7dni', 'procent_poz_testow'),
-    ("liczba_wykonanych_testow_srednia_7dni", 'liczba_wykonanych_testow'),
-    ('liczba_na_10_tys_mieszkancow_7dni', "liczba_na_10_tys_mieszkancow")
-]
-columns_to_date_type = [
-    ("stan_rekordu_na_str", "stan_rekordu_na_str")
-]
-
-columns_to_percentage = [
-    ('procent_poz_testow', ('liczba_testow_z_wynikiem_pozytywnym', 'liczba_wykonanych_testow'))
-]
-
-index_column = "stan_rekordu_na_str"
-
-operation_order = [
-    "convert_to_int", "convert_to_str", "convert_to_date",
-    "calculate_percentage", "calculate_avg"
-]
-
-columns = {
-    "index": index_column,
-    "convert_to_int": columns_to_int,
-    "convert_to_str": columns_to_str,
-    "convert_to_date": columns_to_date_type,
-    "calculate_avg": columns_to_average,
-    "calculate_percentage": columns_to_percentage,
-    "plot": columns_to_plot,
-    "operation_order": operation_order
-}
-
-        
 
 def parse_args():
     parser = ArgumentParser(
-        formatter_class=ArgumentDefaultsHelpFormatter
-    )
+        formatter_class=ArgumentDefaultsHelpFormatter)
     parser.add_argument("admin_div", choices=["wojewodztwo", "powiat"], 
         default="powiat", help="Input administrative division type")
     parser.add_argument("territory",
         help="Input territory name for which you want graphs generated")
+    parser.add_argument("--config", default="./config/dataset_config.json")
     parser.add_argument("--delimiter", default=";", 
         help="Set delimiter used in CSV files")
     parser.add_argument("--data-dir", dest="data_dir", default="./data", 
         help="Set path for the directory to download data to")
     return parser.parse_args()
 
-def plot_columns(dataset, columns):
-    plt.style.use("fivethirtyeight")
-    plt.figure(figsize=(16, 9))
-    plt.xlabel("Daty")
-    plt.ylabel("Wartości")
-    title = ",".join(columns)
-    plt.title(title)
-    figManager = plt.get_current_fig_manager()
-    figManager.window.showMaximized()
-    for column in columns:
-        plt.plot(dataset[column])
-    plt.show()
 
 if(__name__ == "__main__"):
     print("Parsing command-line arguments")
@@ -110,12 +46,11 @@ if(__name__ == "__main__"):
         print("Data is current")
 
     print("Loading data")
-    dataset = DataSet(data_path, args.delimiter, territory)
+    dataset = DataSet(data_path, args.delimiter, territory, args.config)
 
     print("Processing data")
-    dataset.generate_additional_metrics(columns)
+    dataset.generate_additional_metrics()
 
     print("Plotting selected columns")
-    for column in columns_to_plot:
-        plot_columns(dataset.data, column)
+    dataset.plot()
     print("Done!")
--- lib/dataset.py
+++ lib/dataset.py
@@ -1,13 +1,18 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
+import json
+
 
 class DataSet(object):
-    def __init__(self, data_path, delimiter, territory) -> None:
+    def __init__(self, data_path, delimiter, territory, config_file_path) -> None:
         self.data_path = data_path
         self.delimiter = delimiter
-        self.full_data = self.load_and_concatenate_data()
         self.territory = territory
+        self.config = DataSetConfig(self.territory.id, config_file_path)
+        self.operations = DataSetOperations()
+        self.full_data = self.load_and_concatenate_data()
         self.data = self.get_single_area_data()
 
     def load_and_concatenate_data(self):
@@ -36,27 +41,97 @@ class DataSet(object):
         single_area_data = placeholder.copy()
         return single_area_data
     
-    def convert_to_int(self, source_column_name, result_column_name):
-        self.data[result_column_name] = self.data[source_column_name].astype(int)
+    def generate_additional_metrics(self):
+        self.data.dropna(axis=0, how='any', inplace=True)
 
-    def convert_to_str(self, source_column_name, result_column_name):
-        self.data[result_column_name] = self.data[source_column_name].astype(str)
+        for operation in self.config.operation_order:
+            for operation_config in getattr(self.config, operation):
+                operation_function = self.operations.get_operation_function(operation)
+                if(not operation_function):
+                    raise ValueError(f"Unable to match operation processor method \
+                        for operation: {operation}")
+                result_column_name = operation_config["result_column"]
+                source_column_names = operation_config["source_column"]
+                operation_function(self.data, source_column_names, result_column_name)
 
-    def convert_to_date(self, source_column_name, result_column_name):
-        self.data[result_column_name] = self.data[source_column_name].astype("datetime64")
+        self.data.set_index(self.config.index_column, inplace=True)
+    
+    def plot(self):
+        for plot in self.config.plots:
+            plot.draw(self.data)
 
-    def calculate_percentage(self, source_column_names, result_column_name):
+class DataSetConfig(object):
+    def __init__(self, dataset_name, config_file_path) -> None:
+        self.dataset_name = dataset_name
+        self.from_file = self._load_config_from_file(config_file_path)
+        self.plots = self._build_plot_objects()
+
+
+    def _load_config_from_file(self, file_path):
+        with open(file_path, "r") as json_file:
+            config = json.load(json_file)
+
+        self.plots_data = config["plots"]
+        self.convert_to_int = config["convert_to_int"]
+        self.convert_to_str = config["convert_to_str"]
+        self.calculate_avg = config["calculate_avg"]
+        self.convert_to_date = config["convert_to_date"]
+        self.calculate_percentage = config["calculate_percentage"]
+        self.index_column = config["index_column"]
+        self.operation_order = config["operation_order"]
+
+    def _build_plot_objects(self):
+        plots = []
+        for plot in self.plots_data:
+            title = self.dataset_name +" - "+ plot["name"]
+            columns = plot["columns"]
+            plots.append(Plot(title, columns))
+        return plots
+
+
+class Plot(object):
+    def __init__(self, name, columns) -> None:
+        self.name = name
+        self.columns = columns
+    
+    def draw(self, data):
+        plt.style.use("fivethirtyeight")
+        plt.figure(figsize=(16, 9))
+        plt.xlabel("Daty")
+        plt.ylabel("Wartości")
+        title = self.name
+        plt.title(title)
+        figManager = plt.get_current_fig_manager()
+        figManager.window.showMaximized()
+        for column in self.columns:
+            plt.plot(data[column])
+        plt.show()
+
+    
+class DataSetOperations(object):
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def convert_to_int(self, data, source_column_name, result_column_name):
+        data[result_column_name] = data[source_column_name].astype(int)
+
+    def convert_to_str(self, data, source_column_name, result_column_name):
+        data[result_column_name] = data[source_column_name].astype(str)
+
+    def convert_to_date(self, data, source_column_name, result_column_name):
+        data[result_column_name] = data[source_column_name].astype("datetime64")
+
+    def calculate_percentage(self, data, source_column_names, result_column_name):
         numerator = source_column_names[0]
         denominator = source_column_names[1]
-        self.data[result_column_name] = (
-            self.data[numerator] / self.data[denominator] * 100
+        data[result_column_name] = (
+            data[numerator] / data[denominator] * 100
         ).astype(float)
 
-    def calculate_7_day_rolling_average(self, source_column_name, result_column_name):
-        self.data[result_column_name] = (
-                self.data[source_column_name].rolling(window=7).mean()
+    def calculate_7_day_rolling_average(self, data, source_column_name, result_column_name):
+        data[result_column_name] = (
+                data[source_column_name].rolling(window=7).mean()
             )
-
 
     def get_operation_function(self, operation):
         operations = {
@@ -67,19 +142,3 @@ class DataSet(object):
             "calculate_avg": self.calculate_7_day_rolling_average
         }
         return operations.get(operation, None)
-    
-    def generate_additional_metrics(self, columns_config):
-        self.data.dropna(axis=0, how='any', inplace=True)
-
-        for operation in columns_config['operation_order']:
-            for column in columns_config[operation]:
-                operation_function = self.get_operation_function(operation)
-                if(not operation_function):
-                    raise ValueError(f"Unable to match operation processor method \
-                        for operation: {operation}")
-                result_column_name = column[0]
-                source_column_names = column[1]
-                data = operation_function(source_column_names, result_column_name)
-
-        self.data.set_index(columns_config["index"], inplace=True)
-        return data
